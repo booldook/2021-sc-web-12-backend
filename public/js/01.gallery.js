efa764997ea79f32ec19556c0e82504e029b6c38
booldook <booldook@gmail.com>
2021-05-26 10:24:04 +0900

    반복문

--- public/js/01.gallery.js
+++ public/js/01.gallery.js
@@ -12,6 +12,7 @@ $('.thumb').eq(0).trigger('click');
 function Gallery(parent, imgs) {
 	this.$parent = $(parent);
 	this.imgs = imgs;
+	this.idx = 0;
 	var html = '';
 	html += '<div class="gallery-wrapper">';
 	html += '<div class="img-wrap">';
@@ -30,9 +31,22 @@ function Gallery(parent, imgs) {
 	this.$galleryWrapper.find('.thumb').click(function() {
 		var src = $(this).find('img').attr('src');
 		$(this).parent().prev().find('img').attr('src', src);
+		Gallery.idx = $(this).index();
+		console.log(Gallery.idx);
 	});
 
-	this.$galleryWrapper.find('.thumb').eq(0).trigger('click');
+	this.$galleryWrapper.find('.thumb').eq(this.idx).trigger('click');
+
+	Gallery.prototype.onPrev = function() {
+		this.idx = this.idx === 0 ? this.imgs.length - 1 : this.idx - 1;
+		this.$galleryWrapper.find('.thumb').eq(this.idx).trigger('click');
+	}
+	
+	Gallery.prototype.onNext = function() {
+		this.idx = this.idx === this.imgs.length - 1 ? 0 : this.idx + 1;
+		this.$galleryWrapper.find('.thumb').eq(this.idx).trigger('click');
+	}
+
 	return this;
 }
 
@@ -44,14 +58,28 @@ var gallery = new Gallery('.wrapper', [
 	'../img/gallery/3360.jpg'
 ]);
 
-var gallery = new Gallery('.wrapper2', [
+$('.gallery1 .bt-prev').click(function(){
+	gallery.onPrev();
+});
+
+$('.gallery1 .bt-next').click(function(){
+	gallery.onNext();
+});
+
+
+
+// console.log(gallery.$parent);
+// console.log(gallery.$galleryWrapper);
+// console.log(gallery.imgs);
+
+var gallery2 = new Gallery('.wrapper2', [
 	'../img/gallery/3348.jpg', 
 	'../img/gallery/3334.jpg', 
 	'../img/gallery/3360.jpg',
 	'../img/gallery/3360.jpg',
 ]);
 
-var gallery = new Gallery('.wrapper3', [
+var gallery3 = new Gallery('.wrapper3', [
 	'../img/gallery/3360.jpg',
 	'../img/gallery/3348.jpg', 
 ]);
