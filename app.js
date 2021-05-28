d29ce322ad60aa49ff11108555281cf74f9ce4d5
booldook <booldook@gmail.com>
2021-05-28 13:30:31 +0900

    gbook.ejs

--- app.js
+++ app.js
@@ -9,25 +9,36 @@ app.set('view engine', 'ejs'); // view engine으로 ejs를 쓰겠다.
 app.set('views', path.join(__dirname, './views')); // pug파일 위치
 app.locals.pretty = true; // 브라우저에 보내주는 html을 이쁘게
 
+/* ************ Global variables ************ */
+const event = [
+	{id: 1, title: '좋은이벤트1', src:'evt01.jpg', price: '20,000원'},
+	{id: 2, title: '좋은이벤트2', src:'evt02.jpg', price: '25,000원'},
+	{id: 3, title: '좋은이벤트3', src:'evt03.jpg', price: '30,000원'},
+	{id: 4, title: '좋은이벤트4', src:'evt04.jpg', price: '35,000원'},
+	{id: 5, title: '좋은이벤트5', src:'evt05.jpg', price: '38,000원'},
+	{id: 6, title: '좋은이벤트6', src:'evt06.jpg', price: '39,000원'},
+	{id: 7, title: '좋은이벤트7', src:'evt07.jpg', price: '29,000원'},
+	{id: 8, title: '좋은이벤트8', src:'evt08.jpg', price: '49,000원'},
+	{id: 9, title: '좋은이벤트9', src:'evt09.jpg', price: '89,000원'},
+];
+
+const gbook = [
+	{id: 2, content: '좋아요 좋아요~', createdAt: '2021-05-28 13:25:19'},
+	{id: 1, content: '좋아요~', createdAt: '2021-05-28 13:25:17'},
+];
+
 /* ************ Rounter Static ************ */
 app.use('/', express.static(path.join(__dirname, './public')));
 
 /* ************ Rounter Dynamic ************ */
 app.get('/event', (req, res, next) => {
-	const event = [
-		{id: 1, title: '좋은이벤트1', src:'evt01.jpg', price: '20,000원'},
-		{id: 2, title: '좋은이벤트2', src:'evt02.jpg', price: '25,000원'},
-		{id: 3, title: '좋은이벤트3', src:'evt03.jpg', price: '30,000원'},
-		{id: 4, title: '좋은이벤트4', src:'evt04.jpg', price: '35,000원'},
-		{id: 5, title: '좋은이벤트5', src:'evt05.jpg', price: '38,000원'},
-		{id: 6, title: '좋은이벤트6', src:'evt06.jpg', price: '39,000원'},
-		{id: 7, title: '좋은이벤트7', src:'evt07.jpg', price: '29,000원'},
-		{id: 8, title: '좋은이벤트8', src:'evt08.jpg', price: '49,000원'},
-		{id: 9, title: '좋은이벤트9', src:'evt09.jpg', price: '89,000원'},
-	]
 	res.render('event', { event, pageTitle: '이벤트 사이트' });
 });
 
 app.get('/gbook', (req, res, next) => {
-	res.render('gbook', {pageTitle: '방명록 서비스'});
+	res.render('gbook', {pageTitle: '방명록 서비스', gbook});
+});
+
+app.post('/gbook/save', (req, res, next) => {
+	res.send('저장됨');
 });
