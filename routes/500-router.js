0efe27fbd884413cecca7513a8cb4d2278a6cd8e
booldook <booldook@gmail.com>
2021-05-31 15:31:33 +0900

    예외처리 화면 구현 완료

--- routes/500-router.js
+++ routes/500-router.js
@@ -2,5 +2,11 @@ module.exports = (err, req, res, next) => {
 	// 모든 에러의 종착점
 	console.log('===========ERROR==========');
 	// res.json({ code: err.status, message: err.message });
-	res.render('error/error', { status: err.status, message: err.message });
+	const ejs = { 
+		status: err.status === 404 ? 404 : 500, 
+		message: err.message, 
+		pageTitle: `ERROR ${err.status === 404 ? 404 : 500}`,
+		description: err.description || err.message
+	}
+	res.render('error/error', ejs);
 };
