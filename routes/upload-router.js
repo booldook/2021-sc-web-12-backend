cb1eacf7f64b61f955f7c6b2e57d563c3c8013a4
booldook <booldook@gmail.com>
2021-06-01 15:08:26 +0900

    multer folder 변경

--- routes/upload-router.js
+++ routes/upload-router.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
+const path = require('path');
+const fs = require('fs-extra');
 const multer = require('multer');
-const upload = multer({ dest: 'storages/' })
+const storage = multer.diskStorage({
+	destination: async (req, file, cb) => {
+		try {
+			let folder = path.join(__dirname, '../storages', moment().format('YYMMDD'));
+			await fs.ensureDir(folder);
+			cb(null, folder);
+		}
+		catch(err) {
+			cb(err);
+		}
+	},
+	filename: async (req, file, cb) => {
+		let ext = path.extname(file.originalname);
+		cb(null, file.fieldname + '-' + Date.now() + ext);
+	}
+})
+const upload = multer({ storage });
 
 router.post('/save', upload.single('upfile'), (req, res, next) => {
 	res.json({ body: req.body, file: req.file });
 });
 
 module.exports = router;
+
